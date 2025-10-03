d5815b2153b79167a7c9fcc36ebf006d64e3ee2d
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-10-04 01:03:31 +0530

    error resolve

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Navbar from "./Components/Navbar";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white py-3 shadow-2xl'>
+      Navbar
+    </div>
+  
+  )
+}
+
+export default Navbar
